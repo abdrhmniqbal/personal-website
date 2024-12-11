f17cf6e49923112432b028ec27e6946183e33116
Iqbal Abdurrahman <iqbalabdurrahman04@gmail.com>
2024-12-11 14:34:54 +0700

    Add robots.txt and sitemap

---
+++ app/routes/robots[.]txt.ts
@@ -0,0 +1,21 @@
+import { createDomain } from '@/lib/utils/http'
+import { generateRobotsTxt } from '@forge42/seo-tools/robots'
+import type { Route } from './+types/robots[.]txt'
+
+export async function loader({ request, context }: Route.LoaderArgs) {
+  const { env } = context
+  const isProductionDeployment = env.APP_MODE === 'production'
+  const domain = createDomain(request)
+  const robotsTxt = generateRobotsTxt([
+    {
+      userAgent: '*',
+      [isProductionDeployment ? 'allow' : 'disallow']: ['/'],
+      sitemap: [`${domain}/sitemap.xml`],
+    },
+  ])
+  return new Response(robotsTxt, {
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+  })
+}
---
+++ app/routes/sitemap[.]xml.ts
@@ -0,0 +1,24 @@
+import { createDomain } from '@/lib/utils/http'
+import { generateRemixSitemap } from '@forge42/seo-tools/remix/sitemap'
+import type { Route } from './+types/sitemap[.]xml'
+
+export const loader = async ({ request, params }: Route.LoaderArgs) => {
+  const domain = createDomain(request)
+
+  // @ts-expect-error - This import exists but is not picked up by the typescript compiler because it's a remix internal
+  const { routes } = await import('virtual:react-router/server-build')
+
+  const sitemap = await generateRemixSitemap({
+    domain,
+    routes,
+    ignore: ['/resource/*', '/action/*', '/post/*'],
+    // Transforms the url before adding it to the sitemap
+    urlTransformer: (url) => `${url}`,
+  })
+
+  return new Response(sitemap, {
+    headers: {
+      'Content-Type': 'application/xml; charset=utf-8',
+    },
+  })
+}
