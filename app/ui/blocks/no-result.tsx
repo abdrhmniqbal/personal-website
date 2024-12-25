a387ffd4c90ab3d8ef0042b0f22c9d75ccf426f7
iblabd <iqbalabdurrahman04@gmail.com>
2024-12-25 15:23:39 +0700

    Fix wrong text

--- app/ui/blocks/no-result.tsx
+++ app/ui/blocks/no-result.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router'
 import { cn } from '@/lib/utils/css'
+import { toTitleCase } from '@/lib/utils/string'
 import { buttonStyles } from '@/ui/components/button'
 import { Heading } from '@/ui/components/heading'
 
@@ -10,13 +11,13 @@ export default function NoResult({ item }: { item: string }) {
         <Heading level={2}>No result found</Heading>
         <p className="text-muted-fg text-sm">There are no {item} to display.</p>
         <Link
-          to="/posts"
+          to={`/${item}`}
           className={cn(
             buttonStyles({ appearance: 'outline', size: 'small' }),
             'mt-4',
           )}
         >
-          Back to Posts
+          Back to {toTitleCase(item)}
         </Link>
       </div>
     </div>
